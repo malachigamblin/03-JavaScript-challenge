681c650259c352ea7afe87167a3a8a9740707ded
malachigamblin <malachi.gamblin@gmail.com>
2023-02-09 11:09:57 -0500

    added function to prompt for user to enter password

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 // Assignment code here
+const generateButton = document.getElementById("generate");
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateButton.addEventListener("click", function () {
+  // Prompt for password length
+  let length = parseInt(prompt("Enter password length (8-128 characters):"));
+  while (length < 8 || length > 128 || isNaN(length)) {
+    length = parseInt(prompt("Password length must be between 8 and 128 characters. Enter password length:"));
+  }
+})
