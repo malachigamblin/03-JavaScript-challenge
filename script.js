ce73aed12e1d5955e3c454b29dc44b21b544882e
malachigamblin <malachi.gamblin@gmail.com>
2023-02-09 22:05:22 -0500

    validated that user has to select an input

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ generateButton.addEventListener("click", function () {
   while (length < 8 || length > 128 || isNaN(length)) {
     length = parseInt(prompt("Password length must be between 8 and 128 characters. Enter password length:"));
   }
+
+  let includeLowercase = confirm("Include lowercase letters?");
+  let includeUppercase = confirm("Include uppercase letters?");
+  let includeNumbers = confirm("Include numbers?");
+  let includeSpecial = confirm("Include special characters?");
+
+
+while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+  alert("You must select at least one character type. Please try again.");
+  includeLowercase = confirm("Include lowercase letters?");
+  includeUppercase = confirm("Include uppercase letters?");
+  includeNumbers = confirm("Include numbers?");
+  includeSpecial = confirm("Include special characters?");
+}
 })
